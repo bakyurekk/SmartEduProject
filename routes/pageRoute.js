429e9fa9d89775ae55d61c247715d10b9ee3e475
burkanakyurek <burkanakyurek@hotmail.com>
2021-06-04 18:41:26 +0300

    Third commit

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const pageController = require('../controller/pageController');
+const pageController = require('../controllers/pageController');
 
-router.get('/about', pageController.getAboutPage);
 router.get('/', pageController.getIndexPage);
+router.get('/about', pageController.getAboutPage);
+router.get('/courses', pageController.getCoursesPage);
+router.get('/dashboard', pageController.getDashboardPage);
+router.get('/contact', pageController.getContactPage);
   
   
   
